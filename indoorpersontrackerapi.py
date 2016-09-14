68d22f7ebec38a1bdf8c555ac50f9f9b8ed2a2bd
ElD4n1 <=>
2016-09-14 09:30:33 +0000

    updated to new web service interface

--- indoorpersontrackerapi.py
+++ indoorpersontrackerapi.py
@@ -8,10 +8,19 @@ class IndoorPersonTrackerAPI:
         self.client = Client(url)
 
     def register(self, identifier):
-        self.client.service.register(identifier)
-	
-    def updateDetection(self, identifier, probFalseDetection):
-        self.client.service.updateDetection(identifier, probFalseDetection)
+        return self.client.service.register(identifier)
 
-    def updateIdentification(self, identifier, personName, probFalseDetection):
-        self.client.service.updateIdentification(identifier, personName, probFalseDetection)
+    def deregister(self, identifier):
+        self.client.service.deregister(identifier)
+
+    def updateDetection(self, identifier):
+        self.client.service.updateDetection(identifier)
+
+    def updateDetectionCustomPFD(self, identifier, probFalseDetection):
+        self.client.service.updateDetectionCustomPFD(identifier, probFalseDetection)
+
+    def updateIdentification(self, identifier, personName):
+        self.client.service.updateIdentification(identifier, personName)
+
+    def updateIdentificationCustomPFD(self, identifier, personName, probFalseDetection):
+        self.client.service.updateIdentificationCustomPFD(identifier, personName, probFalseDetection)
